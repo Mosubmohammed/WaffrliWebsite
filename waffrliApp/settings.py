856d2f9014fcf9054418068e5d2bb11fbbea5215
Mosubmohammed <mosup.050@gmail.com>
2025-04-09 20:21:52 +0300

    trying to  con aws

--- waffrliApp/settings.py
+++ waffrliApp/settings.py
@@ -85,22 +85,25 @@ WSGI_APPLICATION = 'waffrliApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # Use the default database instead
+        'USER': 'Mosab',
+        'PASSWORD': '3Nksory_2309',
+        'HOST': 'wafrlliwebsite.cts24g66cvk9.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
-# DATABASES = {   
-#              'default': { 
-#                  'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#                  'NAME': 'waffrlidb',       'USER': 'postgres',
-#                  'PASSWORD': '3Nksory_2309',
-#                  'HOST': '127.0.0.1',
-#                  'PORT': '5432',   
-#                  }
-#              }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
