0334b80e23bc6733aff67ee9cf5823b3f5fa96af
Mosubmohammed <mosup.050@gmail.com>
2025-04-12 20:25:13 +0300

    trying to  conn firebase

--- waffrli/models.py
+++ waffrli/models.py
@@ -3,8 +3,7 @@ from django.utils import timezone
 from django.db.models.signals import post_save
 from django.db import models
 from django.contrib.auth.models import User
-from django.dispatch import receiver
-# Create your models here.
+
 
 from django.contrib.auth import get_user_model
 
