f9f14bd29ac5eafbcd1baeb40584c66a09726f80
Mosubmohammed <mosup.050@gmail.com>
2025-04-16 09:05:48 +0300

    not so many problems

--- waffrliApp/settings.py
+++ waffrliApp/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'waffrliApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',  # Use the default database instead
-        'USER': 'Mosab',
-        'PASSWORD': '3Nksory_2309',
-        'HOST': 'wafrlliwebsite.cts24g66cvk9.eu-north-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',  # Use the default database instead
+#         'USER': 'Mosab',
+#         'PASSWORD': '3Nksory_2309',
+#         'HOST': 'wafrlliwebsite.cts24g66cvk9.eu-north-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
