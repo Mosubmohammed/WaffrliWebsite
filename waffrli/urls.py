92c3459c9b64c0003b702f92933efed5f4309e9b
Mosubmohammed <mosup.050@gmail.com>
2025-05-07 20:29:07 +0300

    Descriptive message about your changes

--- waffrli/urls.py
+++ waffrli/urls.py
@@ -43,5 +43,7 @@ urlpatterns = [
     path('archived-deals/<int:deal_id>/continue/',continue_archived_deal, name='continue_archived_deal'),
     path('archived-deals/<int:deal_id>/delete/',delete_archived_deal, name='delete_archived_deal'),
     path('archived-deals/<int:deal_id>/publish/',publish_archived_deal, name='publish_archived_deal'),
+    path('deal/<int:deal_id>/archive/', archive_deal, name='archive_deal'),
+
     path('product/<int:product_id>/report/', report_deal, name='report_deal'),
 ]
